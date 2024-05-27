af262d0166f31be85505fe124dd580b63441604f
JeanCarlos <jmatochepascual@gmail.com>
2024-05-27 06:40:40 -0500

    [ADD] selectores js

---
+++ js/selectores.js
@@ -0,0 +1,4 @@
+const nombreInputElement = document.querySelector('#nombre');
+const emailInputElement = document.querySelector('#email');
+const telefonoInputElement = document.querySelector('#telefono');
+const empresaInputElement = document.querySelector('#empresa');
